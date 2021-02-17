c9399a7e6c8a975be87e4d78e009ecfd781031aa
Jesús <16318863+jesusninoc@users.noreply.github.com>
2021-02-17 11:00:28 +0100

    Update and rename getindex.php to getindex.md

--- GetPost/getindex.php
+++
@@ -1,5 +0,0 @@
-<form action="exampleget.php" method="get">
-    Nombre: <input type="text" name="nombre" /><br />
-    Edad: <input type="text" name="edad" /><br />
-    <input type="submit" name="submit" value="Enviar" />
-</form>
